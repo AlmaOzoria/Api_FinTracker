da91be389f91c725c311c60205d143ae5564a9da
Alma Ozoria <rosaalma624@gmail.com>
2025-08-02 02:06:34 -0400

    se crearon los totales

---
+++ Data/Models/TotalAnual.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Data.Models;
+
+public class TotalAnual
+{
+    public int Ano { get; set; }   
+    public double Total { get; set; }
+}
---
+++ Data/Models/TotalMes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models;
+
+public class TotalMes
+{
+    public string Mes { get; set; }  
+    public double Total { get; set; }
+}
